37824b346730a623b8ad8f64f6882231b793423f
Sanja <sanja.pistac@gmail.com>
2023-01-08 22:30:37 +0100

    Two tasks finished in BuiltInObjects exercise.

--- BuiltInObjects/main.js
+++ BuiltInObjects/main.js
@@ -22,3 +22,31 @@ function duplicateEachElementOfTheArray(array) {
 var duplicate = duplicateEachElementOfTheArray([2, 4, 7, 11, -2, 1]);
 
 console.log(duplicate);
+
+
+/**2. Write a functional expression that removes all duplicates in a given array.
+Input: [8, 13, 8, 9, 12, 8, 1, 1, 4, 13]
+Output: [1, 4, 8, 9, 12, 13] */
+
+function removeAllDuplicatesFromTheArray (array) {
+
+    var newArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+
+        if(newArray.indexOf(array[i]) === -1) {
+
+            newArray.push(array[i]);
+        }
+    }
+    
+    return newArray.sort(function (a, b) {
+
+        return a - b;
+        
+        })
+}
+
+var removeDuplicates =  removeAllDuplicatesFromTheArray ([8, 13, 8, 9, 12, 8, 1, 1, 4, 13]);
+
+console.log(removeDuplicates);
