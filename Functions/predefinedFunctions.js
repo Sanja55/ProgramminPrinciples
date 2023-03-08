47796756d6247c2955395f99c06533370bc400a9
Sanja <sanja.pistac@gmail.com>
2023-03-08 17:03:50 +0100

    Never forget to push completed tsaks...

--- Functions/predefinedFunctions.js
+++ Functions/predefinedFunctions.js
@@ -49,3 +49,80 @@ var joinAll = joinAllElements ([NaN, 0, 15, false, -22, "", undefined, 47, null]
 
 console.log(joinAll);
 
+
+/**3. Write a program to filter out falsy values from the array.
+Input: [NaN, 0, 15, false, -22, "", undefined, 47, null]
+Output: [15, -22, 47] */
+
+
+function filterOutFalsyValues (array) {
+
+    var filteredArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+
+        if ( !!array[i]) {
+
+            filteredArray[filteredArray.length] = array[i];
+        }
+    }
+
+    return filteredArray;
+}
+
+var filterTheArrray = filterOutFalsyValues ([NaN, 0, 15, false, -22, "", undefined, 47, null]);
+
+console.log(filterTheArrray);
+
+
+/**4. Write a program that calculates a number of integer values in the array.
+Input: [NaN, 23.1, 15, false, -22.5, "", 4, 7, null]
+Output: 3 */
+
+function calculateIntegers (array) {
+
+    var integers = [];
+
+    for (var i = 0; i < array.length; i++) {
+
+       if(parseInt(array[i]) === array[i]) {
+
+        integers[integers.length] = array[i];
+       
+        }
+
+    }
+
+    return integers.length;
+
+}
+
+var integerCounter = calculateIntegers ([NaN, 23.1, 15, false, -22.5, "", 4, 7, null]);
+
+console.log(integerCounter);
+
+/** 5. Write a program that calculates a number of float values in the array.
+Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
+Output: 2 */
+
+function countFloats(array) {
+    
+    var floats = [];
+    
+    for (var i = 0; i < array.length; i++) {
+        
+        if (typeof array[i] === "number" && array[i] !== parseInt(array[i]) && isNaN(array[i]) === false) {
+          
+            output[output.length] = array[i];
+        } 
+    
+    }
+    
+    return floats.length;
+  
+}
+         
+  var floatsCounter = countFloats([NaN, 23.1, 15, false, -22.5, '', 4, 7, null]);
+  
+  console.log(floatsCounter);
+
