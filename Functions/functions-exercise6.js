92b82ac795288ab8def0736b33fb134da2211709
Sanja <sanja.pistac@gmail.com>
2023-03-09 23:43:42 +0100

    Frames can wait...

--- Functions/functions-exercise6.js
+++ Functions/functions-exercise6.js
@@ -358,6 +358,78 @@ function greaterThanAverage (array) {
 console.log(greaterThanAverage([3, 5, 7, 8, 12, 14, 17, 22]));
 
 
+/** 14. The body mass index (BMI) is the ratio of the weight of a person (in kilograms) to the
+square of the height (in meters). Write a function that takes two parameters, weight and
+height, computes the BMI, and prints the corresponding BMI category:
+● Starvation: less than 15
+● Anorexic: less than 17.5
+● Underweight: less than 18.5
+● Ideal: greater than or equal to 18.5 but less than 25
+● Overweight: greater than or equal to 25 but less than 30
+● Obese: greater than or equal to 30 but less than 40
+● Morbidly obese: greater than or equal to 40 */
+
+
+function computeBMI(weight, height) {
+    
+    var bmiResult;
+    var category;
+    
+    bmiResult = weight / (height * height);
+    
+    if (bmiResult < 15) {
+        category = "Starvation";
+    }
+    
+    if (bmiResult < 17.5) {
+        category = "Anorexic";
+    }
+    
+    if (bmiResult < 18.5) {
+        category = "Underweight";
+    }
+    
+    if (bmiResult >= 18.5 && bmiResult < 25){
+        category = "Ideal";
+    }
+    
+    if (bmiResult >= 25 && bmiResult < 30) {
+        category = "Overweight";
+    }
+    
+    if (bmiResult >= 30 && bmiResult < 40) {
+        category = "Obese";
+    }
+    
+    if (bmiResult >= 40) {
+        category = "Morbidly obese";
+    }
+    
+    return category;
+}
+ 
+var bmiMath = computeBMI(95, 1.88);
+
+console.log(bmiMath);
+
+
+
+
+/** 15. Write a function that takes a list of strings and prints them, one per line, in a rectangular
+frame.:
+For example the list ["Hello", "World", "in", "a", "frame"] gets
+printed as:
+*********
+* Hello *
+* World *
+* in *
+* a *
+* frame *
+*********
+*/
+
+
+
 
 
 
